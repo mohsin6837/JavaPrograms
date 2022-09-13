01acdc859ccae7022b0febc1520878c96a9a7a48
mohsin6837 <68821359+mohsin6837@users.noreply.github.com>
2022-09-13 21:52:54 +0530

    Add files via upload

---
+++ Import.java
@@ -0,0 +1,9 @@
+
+import mypackage.Demo;
+public class Import {
+    public static void main(String[] args) {
+        Demo d1=new Demo();
+        System.out.print("I am imported from ");
+        d1.display();
+    }
+}
